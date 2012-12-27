ff2d9fa8dc7d5052ec682de32bf33c149e9bdf1f
nrodrigues <nelson@nelsonjrodrigues.com>
2012-12-27 00:40:19 +0000

    remove unused exception

--- twitter-services/src/main/java/com/nelsonjrodrigues/twitter/services/exceptions/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.nelsonjrodrigues.twitter.services.exceptions;
-
-public class ResourceNotFoundException extends RuntimeException {
-
-	private static final long serialVersionUID = 1L;
-
-}
